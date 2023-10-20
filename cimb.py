966e05ebf4ec2f96bb3feb2078fb30c02fc3b001
J-494 <68671153+J-494@users.noreply.github.com>
2023-10-21 05:56:03 +0800

    Update cimb.py

--- cimb.py
+++ cimb.py
@@ -1,8 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service as ChromeService 
-from subprocess import CREATE_NO_WINDOW
 import time
 
 options = webdriver.ChromeOptions()
